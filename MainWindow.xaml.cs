42e38ca859a4e55db5df036017af1928e08362c2
opanforever1992 <opanforever1992@gmail.com>
2022-03-27 23:04:49 +0300

    new_update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Login_and_Regiser
 {
@@ -31,9 +20,42 @@ namespace Login_and_Regiser
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
-        void hyperlink_Click(object sender, RoutedEventArgs e)
+        private void Reistration_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(@"C:\Firefox\Firefox\firefox.exe", "https://forum.new-sense.ru/register");
         }
+        private void ForgotPass_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(@"C:\Firefox\Firefox\firefox.exe", "https://forum.new-sense.ru/lostpassword/");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TB_login.Text.Trim();
+            string pass = PB_password.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                BorderLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                BorderPass.Background = Brushes.DarkRed;
+                BorderLogin.Background = Brushes.GhostWhite;
+            }
+            else
+            {
+                TB_login.ToolTip = "";
+                BorderLogin.Background = Brushes.GhostWhite;
+                PB_password.ToolTip = "";
+                BorderPass.Background = Brushes.GhostWhite;
+
+                MessageBox.Show("Ok");
+            }
+
+        }
+
+        private void TB_login_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => TB_login.Text = "";
+        private void PB_password_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => PB_password.Password = "";
     }
 }
